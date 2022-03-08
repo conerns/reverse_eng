d07d4d1cc1fd8558d0bc44dd7af3accc6de271c3
daddyr1 <daviderendina9@gmail.com>
2022-03-08 16:12:21 +0100

    Fixed Roles User

--- openmrs-core/api/src/main/java/org/openmrs/User.java
+++ openmrs-core/api/src/main/java/org/openmrs/User.java
@@ -53,7 +53,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	
 	private String email;
 	
-	private Set<Role> roles;
+	private UserRoles userRoles = new UserRoles(); 
 	
 	private Map<String, String> userProperties;
 	
@@ -99,7 +99,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 * @return true/false if this user is defined as a super user
 	 */
 	public boolean isSuperUser() {
-		return containsRole(RoleConstants.SUPERUSER);
+		return userRoles.isSuperUser();
 	}
 	
 	/**
@@ -111,26 +111,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 * @return true/false depending on whether user has specified privilege
 	 */
 	public boolean hasPrivilege(String privilege) {
-		
-		// All authenticated users have the "" (empty) privilege
-		if (StringUtils.isEmpty(privilege)) {
-			return true;
-		}
-		
-		if (isSuperUser()) {
-			return true;
-		}
-		
-		Set<Role> tmproles = getAllRoles();
-		
-		// loop over the roles and check each for the privilege
-		for (Role tmprole : tmproles) {
-			if (tmprole.hasPrivilege(privilege)) {
-				return true;
-			}
-		}
-		
-		return false;
+		return userRoles.hasPrivilege(privilege);
 	}
 	
 	/**
@@ -153,19 +134,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 *         is a superUser
 	 */
 	public boolean hasRole(String r, boolean ignoreSuperUser) {
-		if (!ignoreSuperUser && isSuperUser()) {
-			return true;
-		}
-		
-		if (roles == null) {
-			return false;
-		}
-		
-		Set<Role> tmproles = getAllRoles();
-		
-		log.debug("User # {} has roles: {}", userId, tmproles);
-		
-		return containsRole(r);
+		return userRoles.hasRole(r, ignoreSuperUser);
 	}
 	
 	/**
@@ -178,12 +147,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 * <strong>Should</strong> be case insensitive
 	 */
 	public boolean containsRole(String roleName) {
-		for (Role role : getAllRoles()) {
-			if (role.getRole().equalsIgnoreCase(roleName)) {
-				return true;
-			}
-		}
-		return false;
+		return userRoles.containsRole(roleName);
 	}
 	
 	/**
@@ -193,71 +157,27 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 * @return Collection of complete Privileges this user has
 	 */
 	public Collection<Privilege> getPrivileges() {
-		Set<Privilege> privileges = new HashSet<>();
-		Set<Role> tmproles = getAllRoles();
-		
-		Role role;
-		for (Role tmprole : tmproles) {
-			role = tmprole;
-			Collection<Privilege> privs = role.getPrivileges();
-			if (privs != null) {
-				privileges.addAll(privs);
-			}
-		}
-		
-		return privileges;
+		return userRoles.getPrivileges();
 	}
 	
 	// Property accessors
 	
 	/**
-	 * Returns all roles attributed to this user by expanding the role list to include the parents of
-	 * the assigned roles
-	 * 
-	 * @return all roles (inherited from parents and given) for this user
-	 */
-	public Set<Role> getAllRoles() {
-		// the user's immediate roles
-		Set<Role> baseRoles = new HashSet<>();
-		
-		// the user's complete list of roles including
-		// the parent roles of their immediate roles
-		Set<Role> totalRoles = new HashSet<>();
-		if (getRoles() != null) {
-			baseRoles.addAll(getRoles());
-			totalRoles.addAll(getRoles());
-		}
-		
-		log.debug("User's base roles: {}", baseRoles);
-		
-		try {
-			for (Role r : baseRoles) {
-				totalRoles.addAll(r.getAllParentRoles());
-			}
-		}
-		catch (ClassCastException e) {
-			log.error("Error converting roles for user: " + this);
-			log.error("baseRoles.class: " + baseRoles.getClass().getName());
-			log.error("baseRoles: " + baseRoles.toString());
-			for (Role baseRole : baseRoles) {
-				log.error("baseRole: '" + baseRole + "'");
-			}
-		}
-		return totalRoles;
-	}
-	
-	/**
 	 * @return Returns the roles.
 	 */
 	public Set<Role> getRoles() {
-		return roles;
+		return userRoles.getRoles();
 	}
 	
 	/**
 	 * @param roles The roles to set.
 	 */
 	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
+		userRoles.setRoles(roles);
+	}
+
+	public Set<Role> getAllRoles() {
+		return userRoles.getAllRoles();
 	}
 	
 	/**
@@ -267,13 +187,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 * @return Returns this user with the given role attached
 	 */
 	public User addRole(Role role) {
-		if (roles == null) {
-			roles = new HashSet<>();
-		}
-		if (!roles.contains(role) && role != null) {
-			roles.add(role);
-		}
-		
+		userRoles.addRole(role);
 		return this;
 	}
 	
@@ -284,10 +198,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	 * @return this user with the given role removed
 	 */
 	public User removeRole(Role role) {
-		if (roles != null) {
-			roles.remove(role);
-		}
-		
+		userRoles.removeRole(role);
 		return this;
 	}
 	
---
+++ openmrs-core/api/src/main/java/org/openmrs/UserRoles.java
@@ -0,0 +1,154 @@
+package org.openmrs;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openmrs.util.RoleConstants;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserRoles {
+
+	private Set<Role> roles;
+	
+	public UserRoles(){
+		roles = new HashSet<>();
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public boolean hasRole(String r, boolean ignoreSuperUser) {
+		if (!ignoreSuperUser && isSuperUser()) {
+			return true;
+		}
+
+		if (roles == null) {
+			return false;
+		}
+
+		Set<Role> tmproles = getAllRoles();
+
+		//log.debug("User # {} has roles: {}", userId, tmproles);
+
+		return containsRole(r);
+	}
+
+	/**
+	 * Returns all roles attributed to this user by expanding the role list to include the parents of
+	 * the assigned roles
+	 *
+	 * @return all roles (inherited from parents and given) for this user
+	 */
+	public Set<Role> getAllRoles() {
+		// the user's immediate roles
+		Set<Role> baseRoles = new HashSet<>();
+
+		// the user's complete list of roles including
+		// the parent roles of their immediate roles
+		Set<Role> totalRoles = new HashSet<>();
+		if (getRoles() != null) {
+			baseRoles.addAll(getRoles());
+			totalRoles.addAll(getRoles());
+		}
+
+		//log.debug("User's base roles: {}", baseRoles);
+
+		try {
+			for (Role r : baseRoles) {
+				totalRoles.addAll(r.getAllParentRoles());
+			}
+		}
+		catch (ClassCastException e) {
+			//log.error("Error converting roles for user: " + this);
+			//log.error("baseRoles.class: " + baseRoles.getClass().getName());
+			//log.error("baseRoles: " + baseRoles.toString());
+			//for (Role baseRole : baseRoles) {
+			//	log.error("baseRole: '" + baseRole + "'");
+			//}
+		}
+		return totalRoles;
+	}
+	
+	public UserRoles addRole(Role role) {
+		if (roles == null) {
+			roles = new HashSet<>();
+		}
+		if (!roles.contains(role) && role != null) {
+			roles.add(role);
+		}
+
+		return this;
+	}
+
+	public UserRoles removeRole(Role role) {
+		if (roles != null) {
+			roles.remove(role);
+		}
+
+		return this;
+	}
+
+	public boolean containsRole(String roleName) {
+		for (Role role : getAllRoles()) {
+			if (role.getRole().equalsIgnoreCase(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean hasPrivilege(String privilege) {
+
+		// All authenticated users have the "" (empty) privilege
+		if (StringUtils.isEmpty(privilege)) {
+			return true;
+		}
+
+		if (isSuperUser()) {
+			return true;
+		}
+
+		Set<Role> tmproles = getAllRoles();
+
+		// loop over the roles and check each for the privilege
+		for (Role tmprole : tmproles) {
+			if (tmprole.hasPrivilege(privilege)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get <i>all</i> privileges this user has. This delves into all of the roles that a person has,
+	 * appending unique privileges
+	 *
+	 * @return Collection of complete Privileges this user has
+	 */
+	public Collection<Privilege> getPrivileges() {
+		Set<Privilege> privileges = new HashSet<>();
+		Set<Role> tmproles = getAllRoles();
+
+		Role role;
+		for (Role tmprole : tmproles) {
+			role = tmprole;
+			Collection<Privilege> privs = role.getPrivileges();
+			if (privs != null) {
+				privileges.addAll(privs);
+			}
+		}
+
+		return privileges;
+	}
+
+	public boolean isSuperUser() {
+		return containsRole(RoleConstants.SUPERUSER);
+	}
+}
